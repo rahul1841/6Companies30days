6d81e5063e8fdfe605ee94070d32a52e3dc0992e
rahul1841 <118290059+rahul1841@users.noreply.github.com>
2025-01-16 22:37:09 +0530

    day5 q3 done

--- Walmart/day5.cpp
+++ Walmart/day5.cpp
@@ -58,3 +58,31 @@ public:
         return ans;
     }
 };
+
+
+139. Word Break
+
+class Solution {
+public:
+    int t[301][301];
+    bool solve(int i, int j, string s, map<string, bool>& mp){
+        string str = s.substr(i, j-i+1);
+        if(mp.find(str) != mp.end())return true;
+
+        if(t[i][j] != -1)return t[i][j];
+
+        bool ans = false;
+        for(int k = i; k < j; k++){
+            ans = ans || (solve(i, k, s, mp) && solve(k+1, j, s, mp));
+        }
+
+        return t[i][j] = ans;
+    }
+    bool wordBreak(string s, vector<string>& wordDict) {
+        memset(t, -1, sizeof(t));
+        map<string, bool> mp;
+        for(auto s : wordDict)mp[s] = true;
+
+        return solve(0, s.length()-1, s, mp);
+    }
+};
