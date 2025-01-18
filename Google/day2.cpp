e0472793bb029758985dd4d97857038cea8fbc8c
rahul1841 <118290059+rahul1841@users.noreply.github.com>
2025-01-18 21:59:24 +0530

    day2 q2 done google

--- Google/day2.cpp
+++ Google/day2.cpp
@@ -28,3 +28,27 @@ public:
 };
 
 
+354. Russian Doll Envelopes
+
+
+class Solution {
+public:
+    int maxEnvelopes(vector<vector<int>>& E) {
+        sort(E.begin(), E.end(), [](vector<int>& a, vector<int>& b) 
+             -> bool {return a[0] == b[0] ? b[1] < a[1] : a[0] < b[0];});
+        vector<int> dp;
+        for (auto& env : E) {
+            int height = env[1];
+            int left = lower_bound(dp.begin(), dp.end(), height) - dp.begin();
+            if (left == dp.size()) dp.push_back(height);
+            dp[left] = height;
+        }
+        return dp.size();
+    }
+};
+
+
+
+
+
+
