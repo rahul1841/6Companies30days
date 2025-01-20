643f69b92d42bed44b5c4c052a0f498a542b1014
rahul1841 <118290059+rahul1841@users.noreply.github.com>
2025-01-20 21:28:37 +0530

    day4 done

--- Google/day4.cpp
+++ Google/day4.cpp
@@ -130,3 +130,54 @@ public:
  * obj->addWord(word);
  * bool param_2 = obj->search(word);
  */
+
+
+1530. Number of Good Leaf Nodes Pairs
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+ 
+class Solution {
+public:
+    int countPairs(TreeNode* root, int distance) {
+        int result = 0;
+        dfs(root, result, distance);
+        return result;
+    }
+
+private:
+    vector<int> dfs(TreeNode* node, int& result, int distance) {
+        if (!node) return {};
+
+        if (!node->left && !node->right) return {1};
+
+        vector<int> leftDistances = dfs(node->left, result, distance);
+        vector<int> rightDistances = dfs(node->right, result, distance);
+
+        for (int l : leftDistances) {
+            for (int r : rightDistances) {
+                if (l + r <= distance) {
+                    result++;
+                }
+            }
+        }
+        vector<int> currentDistances;
+        for (int l : leftDistances) {
+            if (l + 1 < distance) currentDistances.push_back(l + 1);
+        }
+        for (int r : rightDistances) {
+            if (r + 1 < distance) currentDistances.push_back(r + 1);
+        }
+
+        return currentDistances;
+    }
+};
