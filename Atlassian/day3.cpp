d80e0f22ff7b7059340164ac5a5af54113e3b29e
rahul1841 <118290059+rahul1841@users.noreply.github.com>
2025-01-23 23:37:18 +0530

    day3 q5 done

--- Atlassian/day3.cpp
+++ Atlassian/day3.cpp
@@ -126,3 +126,18 @@ public:
         return val;
     }
 };
+
+
+2211. Count Collisions on a Road
+
+class Solution {
+public:
+    int countCollisions(string D) {
+        int n = D.size(), ans = 0;
+		int i = 0 , j = n-1;
+        while(i<=j and D[i] == 'L') i++;    
+		while(i<=j and D[j] == 'R') j--;    
+        for( ; i<=j ; i++) ans += (D[i] != 'S');  
+        return ans;
+        }
+};
